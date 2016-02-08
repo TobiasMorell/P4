71fd01842399f53d5db8cb4e952fb6a74834341c
TobiasMorell <tmorel14@student.aau.dk>
2016-02-08 08:39:35 +0100

    src/ : added mod outline

---
+++ src/main/java/com/P4/Reference.java
@@ -0,0 +1,16 @@
+package com.P4;
+
+/**
+ * Created by Nete on 05-02-2016.
+ */
+public class Reference {
+    public static String MOD_ID = "obco";
+    public static String MOD_NAME = "ObsidiCode";
+    public static String VERSION = "0.1";
+
+    //Private constructor prevents initialization
+    private Reference ()
+    {
+
+    }
+}
---
+++ src/main/java/com/P4/proxy/CommonProxy.java
@@ -0,0 +1,11 @@
+package com.P4.proxy;
+
+/**
+ * Created by Morell on 05-02-2016.
+ */
+public class CommonProxy {
+    public void registerRenders()
+    {
+
+    }
+}
