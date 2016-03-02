9b2a1d553c96fe90d405eb46c993a565f7945297
Lee Paludan <Leepaludan@gmail.com>
2016-03-02 13:14:27 +0100

    Initialized Project

---
+++ Scanner/src/java/org/Scanner/Main.java
@@ -0,0 +1,9 @@
+package org.Scanner;
+
+public class Main 
+{
+    public static final void main (String[] args)
+    {
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/com/P4/init/CodeBook.java
@@ -0,0 +1,7 @@
+package com.P4.init;
+
+/**
+ * Created by Lee on 08/02/16.
+ */
+public class CodeBook {
+}
