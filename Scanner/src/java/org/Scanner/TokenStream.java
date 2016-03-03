23ce2d405e2eac8f81f4ff4d5c125a6c468aa277
GoatSnail <gedesneglen@gmail.com>
2016-03-03 09:09:46 +0100

    Now i actually added it? o.O

---
+++ Scanner/src/java/org/Scanner/TokenStream.java
@@ -0,0 +1,13 @@
+package org.Scanner;
+
+/**
+ * Created by Arne K. Rasmussen on 03-03-2016.
+ */
+public class TokenStream {
+    public Token CurrentToken;
+
+
+    public Token Peak(){return new Token();}
+
+    public Token PeakTwo(){return new Token();}
+}
