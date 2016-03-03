37150e2ae37ce801f590950b0da3be87104c56d6
GoatSnail <gedesneglen@gmail.com>
2016-03-03 12:49:10 +0100

    TokenPattern and a few tokens implemented

--- Scanner/src/java/org/Scanner/Token.java
+++ Scanner/src/java/org/Scanner/Token.java
@@ -1,5 +1,9 @@
 package org.Scanner;
 
+import com.sun.javafx.fxml.expression.Expression;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 
@@ -11,8 +15,7 @@ public class Token {
         SIGNAL, START, IF, REPEAT, UNTIL, FOREVER, ELSE, VAL,
         PLUS, MINUS, TIMES, DIVIDE, ASSIGN, POWER, SQUARE_ROOT,
         BOOL_AND, BOOL_OR, BOOL_EQ, BOOL_GT, BOOL_LT, BOOL_GTE,
-        BOOL_LTE, BOOL_LIT, COORD_LIT, STRING_LIT, TYPE, CHEST_LIT,
-        BLOCK_LIT, ITEM_LIT, NPC_LIT
+        BOOL_LTE, BOOL_LIT, COORD_LIT, STRING_LIT, TYPE
     }
 
     public final type _type;
@@ -32,11 +35,22 @@ public class Token {
     }
 
     public static class TokenPattern {
-        TokenPattern(type t, Pattern p){
-
+        TokenPattern(type t, String p){
+            tokenType = t;
+            regex = p;
         }
-        type Tokentype;
-        Pattern pattern;
+        public final type tokenType;
+        public final String regex;
     }
+
+    public static final ArrayList<TokenPattern> patternList = new ArrayList<TokenPattern>(Arrays.asList(
+            new TokenPattern(type.ASSIGN,"="),
+            new TokenPattern(type.BOOL_AND,"AND"),
+            new TokenPattern(type.BOOL_DCL,"BOOL"),
+            new TokenPattern(type.BOOL_EQ,"IS"),
+            new TokenPattern(type.BOOL_GT,"GREATER_THAN"),
+            new TokenPattern(type.BOOL_GTE,"GREATER_THAN_EQUAL"),
+            new TokenPattern(type.BOOL_LIT,"TRUE|FALSE")
+    ));
 
 }
