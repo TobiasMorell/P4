9cbf92402d3bc4b4d82e158616507eabd8fb4753
TobiasMorell <tmorel14@student.aau.dk>
2016-03-09 16:15:40 +0100

    AST/ : Added some general nodes and outlined a Visitor pattern

--- AST/ASTLee/src/java/org/AST/Main.java
+++ AST/ASTLee/src/java/org/AST/Main.java
@@ -1,23 +1,15 @@
 package org.AST;
 
-public class Main 
+import org.AST.GeneralNodes.Node;
+import org.AST.SyntaxNodes.PlusNode;
+import org.Visitors.PrettyPrintVisitor;
+
+public class Main
 {
-    public static final void main (String args[])
+    public static void main (String args[])
     {
-        Node a = new Node ("Root", null);
-        Node b = new Node ("LeftMost", a); 
-        Node c = new Node ("RightMost", a);
-
-        a._leftmost = b;
-        a._rightmost = c;
-
-        printTree(a);
-    }
-
-    public static void printTree (Node root)
-    {
-        String space = "     ";
-        System.out.println(space + root);
-        System.out.println(root._leftmost + space + root._rightmost); 
+        PlusNode pn = new PlusNode(null, null);
+        PrettyPrintVisitor ppv = new PrettyPrintVisitor();
+        ppv.Visit(pn);
     }
 }
