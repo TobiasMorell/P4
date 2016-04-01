22dd47a3a32ffd230e458944b8c3c225d779bdc2
GoatSnail <gedesneglen@gmail.com>
2016-04-01 10:22:16 +0200

    Further Cleaned project

--- AST/src/Main.java
+++ AST/src/Main.java
@@ -5,7 +5,7 @@ import Visitors.PrettyPrintVisitor;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!\n\n");
+        System.out.println("Hello Worldd!\n\n");
 
         PlusNode TestNode = new PlusNode(new NumLit("5"),new NumLit("7"));
         PrettyPrintVisitor PPTest = new PrettyPrintVisitor();
