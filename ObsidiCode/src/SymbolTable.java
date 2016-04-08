1203227f96c662973d9a8017466a0288219c83a6
GoatSnail <gedesneglen@gmail.com>
2016-04-08 12:48:52 +0200

    out commented bad line

--- ObsidiCode/src/SymbolTable.java
+++ ObsidiCode/src/SymbolTable.java
@@ -24,7 +24,7 @@ public class SymbolTable {
         }else if(node instanceof ReferenceNode){
             //Symbol = Symboltable.retrieveSymbol(node.name)
             //if(Symbol == null) error
-            elseerror
+            //elseerror
         }
     }
 
