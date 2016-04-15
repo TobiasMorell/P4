361b629a7d37a8eb63267f603264d0fbf25ba787
GoatSnail <gedesneglen@gmail.com>
2016-04-15 08:26:18 +0200

    Func :: Made Parameters a list of types

--- ObsidiCode/src/Func.java
+++ ObsidiCode/src/Func.java
@@ -1,6 +1,7 @@
 import ASTNodes.Declarations.DeclarationNode;
 import ASTNodes.Declarations.MethodDcl;
 import ASTNodes.GeneralNodes.Node;
+import ASTNodes.SyntaxNodes.BlockNode;
 
 import java.util.ArrayList;
 
@@ -9,11 +10,15 @@ import java.util.ArrayList;
  */
 public class Func {
     Node.Type returnType;
-    ArrayList<Symbol> parameters;
+    public ArrayList<Node.Type> parameters;
 
         public Func(MethodDcl methodNode){
+            returnType = methodNode.type;
             for (Node n :methodNode.parameters) {
-                ;
+                DeclarationNode N = (DeclarationNode)n;
+                parameters.add(N.type);
             }
+            //Add the parameters to the beginning of the blockNode so they'll be recorded in the scope for checking.
+            ((BlockNode)methodNode.GetLeftChild()).GetChildren().addAll(0,methodNode.parameters);
         }
 }
