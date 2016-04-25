a3988a92b5877eabc4dbfa599ba00a2ef9ef963d
Arne Rasmussen <Gedesneglen@gmail.com>
2016-04-25 11:01:57 +0200

    Started project to test thread concept
    nothing done though

---
+++ ThreadConceptTest/src/com/company/Code.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Gedesnegl on 22-04-2016.
+ */
+public class Code {
+}
---
+++ ThreadConceptTest/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Main {
+    /***
+     * The Main thread is the Framework that we run our code in.
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello :#");
+}
+
+}
