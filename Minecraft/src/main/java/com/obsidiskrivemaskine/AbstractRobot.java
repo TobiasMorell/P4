144852fa2b5558c5bd557c7f32e9370f52431e87
Esben L. Kirkegaard <esben411@hotmail.com>
2016-04-29 14:58:31 +0100

    Updated Minecraft, added SyncRobot to tie AbstractRobot methods to tickrate.

--- Minecraft/src/main/java/com/obsidiskrivemaskine/AbstractRobot.java
+++ Minecraft/src/main/java/com/obsidiskrivemaskine/AbstractRobot.java
@@ -1,6 +1,7 @@
 package com.obsidiskrivemaskine;
 
-import com.obsidiskrivemaskine.GUI.RobotEntity;
+import com.obsidiskrivemaskine.Entity.RobotEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -16,28 +17,23 @@ import java.util.ArrayList;
  */
 public abstract class AbstractRobot {
     static World world;
+    public static boolean IsRunning;
     static EntityPlayer player;
     private static RobotEntity Robot;
-
-    //This method is called when the block is rightclicked to initialize the world and player.
-    public static void hack(World worldIn, EntityPlayer playerIn){
-        world = worldIn;
-        player = playerIn;
-    }
 
     public static void talk(String msg){
         player.addChatMessage(new ChatComponentText("\247f" + msg));
     }
 
-    public static void move(String direction){
+    public static void move(String direction) {
         if (direction.equalsIgnoreCase("east"))
-            Robot.getNavigator().tryMoveToXYZ(Robot.posX + 1, Robot.posY, Robot.posZ, 0.8);
-        else if(direction.equalsIgnoreCase("west"))
-            Robot.getNavigator().tryMoveToXYZ(Robot.posX - 10, Robot.posY, Robot.posZ, 0.8);
-        else if(direction.equalsIgnoreCase("north"))
-            Robot.getNavigator().tryMoveToXYZ(Robot.posX, Robot.posY, Robot.posZ - 1, 0.8);
-        else if(direction.equalsIgnoreCase("south"))
-            Robot.getNavigator().tryMoveToXYZ(Robot.posX, Robot.posY, Robot.posZ + 1, 0.8);
+            Robot.getNavigator().tryMoveToXYZ(Robot.posX + 1, Robot.posY, Robot.posZ, 1);
+        else if (direction.equalsIgnoreCase("west"))
+            Robot.getNavigator().tryMoveToXYZ(Robot.posX - 1, Robot.posY, Robot.posZ, 1);
+        else if (direction.equalsIgnoreCase("north"))
+            Robot.getNavigator().tryMoveToXYZ(Robot.posX, Robot.posY, Robot.posZ - 1, 1);
+        else if (direction.equalsIgnoreCase("south"))
+            Robot.getNavigator().tryMoveToXYZ(Robot.posX, Robot.posY, Robot.posZ + 1, 1);
     }
 
     public static void dig(String direction) {
@@ -88,12 +84,10 @@ public abstract class AbstractRobot {
         Robot.getHeldItem().onItemUse(player, world, Robot.getPosition().add(0, 0, 1), Robot.getHorizontalFacing(), 0, 0, 0);
     }
 
-    public static void drop(String toDrop, boolean all){
+    public static void drop(String toDrop, int amount){
         ItemStack itemToDrop = searchInventory(toDrop);
-        if (all)
-            Robot.dropItem(itemToDrop.getItem(), itemToDrop.stackSize);
-        else
-            Robot.dropItem(itemToDrop.getItem(), 1);
+        if(itemToDrop != null)
+            Robot.dropItem(itemToDrop.getItem(), amount);
     }
 
     public static void lootChest(){
@@ -107,31 +101,40 @@ public abstract class AbstractRobot {
     }
 
     public static void swing(){
-        if (Robot.getAttackTarget() == null)
-            findNearestEnemy();
-        if(Robot.getDistanceToEntity(Robot.getAttackTarget()) < 1)
-        Robot.attackEntityAsMob(Robot.getAttackTarget());
+        if (Robot.getAttackTarget() == null || Robot.getDistanceToEntity(Robot.getAttackTarget()) > 1.5 || Robot.getAttackTarget().isDead)
+            targetNearestEnemy();
+        if(Robot.getDistanceToEntity(Robot.getAttackTarget()) <= 1.5)
+            Robot.attackEntityAsMob(Robot.getAttackTarget());
     }
-
+/*
     public static void createEntity(){
         Robot = new RobotEntity(world);
     }
 
+
     public static void createEntityIfNotPresent(){
         if(Robot == null || Robot.isDead) {
-            createEntity();
-            spawnEntity();
+            //createEntity();
+            spawnEntity(Robot);
         }
     }
-
-    public static void spawnEntity(){
-        Robot.setLocationAndAngles(((double)(int)player.posX) + 5.5, (double)(int)player.posY, ((double)(int)player.posZ) + 0.5, player.rotationYaw, 0.0F);
+*/
+    public static void spawnEntity(RobotEntity robot, EntityPlayer playerIn, World worldIn){
+        Robot = robot;
+        player = playerIn;
+        world = worldIn;
+        /*robot.setLocationAndAngles(((double)(int)player.posX) + 5.5, (double)(int)player.posY + 1, ((double)(int)player.posZ) + 0.5, player.rotationYaw, 0.0F);
         for(int i = 0; i < 15; i++)
-            Robot.spawnExplosionParticle();
-        world.spawnEntityInWorld(Robot);
+            robot.spawnExplosionParticle();
+        worldIn.spawnEntityInWorld(robot);*/
     }
 
-    public static void findNearestEnemy(){
+    public static void targetNearestEnemy(){
+        if(findNearestEnemy() != null)
+            Robot.setAttackTarget(findNearestEnemy());
+    }
+
+    public static EntityLivingBase findNearestEnemy(){
         if(Robot.getHostileEntities().get(0) != null) {
             ArrayList<EntityLivingBase> enemies = Robot.getHostileEntities();
             EntityLivingBase closest = enemies.get(0);
@@ -139,7 +142,8 @@ public abstract class AbstractRobot {
                 if(ei.getDistanceToEntity(Robot) <= closest.getDistanceToEntity(Robot))
                     closest = ei;
             }
-            Robot.setAttackTarget(closest);
+            return closest;
         }
+        return null;
     }
 }
